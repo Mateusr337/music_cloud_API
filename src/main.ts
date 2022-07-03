41d285f125d41a888ae5e7c4f3ef3f353e80e179
Mateus De Nardo <mateusdnm@hotmail.com>
2022-07-03 03:09:17 +0200

    chore: add docker support

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,6 @@ import { DomainExceptionFilter } from './infra/http/domain-exception.filter';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalFilters(new DomainExceptionFilter());
-  await app.listen(3000);
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
